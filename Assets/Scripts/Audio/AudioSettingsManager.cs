4f9ec07e3de24eecd0a03c3525063f2f7da2a848
DanielTeer <daniel.teer94@gmail.com>
2026-06-14 23:36:51 -0700

    dont put build in assets folder it breaks it

--- Assets/Scripts/Audio/AudioSettingsManager.cs
+++ Assets/Scripts/Audio/AudioSettingsManager.cs
@@ -4,49 +4,49 @@ using UnityEngine.UI;
 
 public class AudioSettingsManager : MonoBehaviour
 {
-    public AudioMixer mixer;
+    public AudioMixer mixer;//labels mixer
 
-    public Slider masterSlider;
+    public Slider masterSlider;//slider names
     public Slider musicSlider;
     public Slider sfxSlider;
 
     void Awake()
     {
-        masterSlider.onValueChanged.AddListener(SetMasterVolume);
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);//sets the listener to max
         musicSlider.onValueChanged.AddListener(SetMusicVolume);
         sfxSlider.onValueChanged.AddListener(SetSFXVolume);
     }
 
     void Start()
     {
-        Invoke(nameof(InitAudio), 0.05f);
+        Invoke(nameof(InitAudio), 0.05f);//lowes setting to prevent bottom out
     }
 
     void InitAudio()
     {
-        SetMasterVolume(masterSlider.value);
+        SetMasterVolume(masterSlider.value);// sets default levels
         SetMusicVolume(musicSlider.value);
         SetSFXVolume(sfxSlider.value);
     }
 
     public void SetMasterVolume(float value)
     {
-        ApplyVolume("MasterVolume", value);
+        ApplyVolume("MasterVolume", value);//takes screen input to mixer
     }
 
     public void SetMusicVolume(float value)
     {
-        ApplyVolume("MusicVolume", value);
+        ApplyVolume("MusicVolume", value);//takes screen input to mixer
     }
 
     public void SetSFXVolume(float value)
     {
-        ApplyVolume("SFXVolume", value);
+        ApplyVolume("SFXVolume", value);//takes screen input to mixer
     }
 
     private void ApplyVolume(string paramName, float value)
     {
-        float db;
+        float db;//cals level to db
 
         if (value <= 0.0001f)
         {
@@ -54,7 +54,7 @@ public class AudioSettingsManager : MonoBehaviour
         }
         else
         {
-            db = Mathf.Log10(value) * 20f;
+            db = Mathf.Log10(value) * 20f;//sets volume in db levels
         }
 
         mixer.SetFloat(paramName, db);
